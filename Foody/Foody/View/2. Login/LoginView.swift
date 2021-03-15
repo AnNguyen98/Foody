b1e4d1d7403a7f051dcf394d6a9eebbf7e4084b2
An Nguyen T <an.nguyen2@monstar-lab.com>
2021-03-15 11:15:24 +0700

    Completion login ui

--- Foody/Foody/View/2. Login/LoginView.swift
+++ Foody/Foody/View/2. Login/LoginView.swift
@@ -8,82 +8,76 @@
 import SwiftUI
 
 struct LoginView: View {
-    @State private var username: String = ""
+    @State private var email: String = ""
     @State private var password: String = ""
     
     var body: some View {
-        ZStack(alignment: .topLeading) {
+        ZStack {
             Image("bg_login")
                 .resizable()
                 .ignoresSafeArea()
-            ScrollView {
-                VStack {
-                    VStack(alignment: .leading) {
-                        Text("Welcome back")
-                            .font(.system(size: 44))
-                        Text("Sign in to continue")
-                            .font(.system(size: 17))
-                    }
-                    .padding(.horizontal)
-                    .padding(.top, 60)
-                    
-                    VStack(spacing: 20) {
-                        TextFieldCustom(text: $username,
-                                        placeholder: Text("Username").foregroundColor(.gray))
-                        
-                        TextFieldCustom(text: $password,
-                                        placeholder: Text("Password").foregroundColor(.gray),
-                                        onCommit: {}, isSecureField: true)
-
-                    }
-                    .font(.system(size: 17))
-                    .padding(EdgeInsets(top: 40, leading: 20, bottom: 60, trailing: 20))
-                    
-                    VStack(spacing: 20) {
-                        Button(action: {}, label: {
-                            Text("Sign In")
-                                .frame(maxWidth: .infinity, minHeight: 50)
-                                .background(Color(#colorLiteral(red: 0.9607843137, green: 0.1764705882, blue: 0.337254902, alpha: 1)))
-                        })
-                        .clipShape(RoundedRectangle(cornerRadius: 8))
-                        
-                        Button(action: {}, label: {
-                            HStack {
-                                Image("facebook_icon")
-                                    .resizable()
-                                    .frame(width: 12, height: 24)
-                                Text("Connect with Facebook")
-                            }
-                            .frame(maxWidth: .infinity, minHeight: 50)
-                            .background(Color(#colorLiteral(red: 0.1490196078, green: 0.4470588235, blue: 0.7960784314, alpha: 1)))
-                            .clipShape(RoundedRectangle(cornerRadius: 8))
-                        })
-                        
-                        Button(action: {}, label: {
-                            Text("Forgot password?")
-                        })
-                        
-                        Spacer()
-                        
-                        HStack {
-                            Text("Don't have an account?")
-                            Button(action: {}, label: {
-                                Text("Sign up")
-                                    .bold()
-                            })
-                        }
-                        .padding(.bottom, 15)
-                    }
-                    .font(.system(size: 15))
-                    .padding(.horizontal)
+            
+            VStack(alignment: .leading) {
+                VStack(alignment: .leading) {
+                    Text("Welcome back")
+                        .bold(size: 44)
+                    Text("Sign in to continue")
+                        .regular(size: 18)
                 }
+                .padding(.horizontal)
+                .padding(.top, 60)
+                
+                VStack(spacing: 30) {
+                    TextFieldCustom(text: $email,
+                                    placeholder: Text("Email").foregroundColor(.gray))
+                    
+                    TextFieldCustom(text: $password,
+                                    placeholder: Text("Password").foregroundColor(.gray),
+                                    onCommit: {
+                                        // TODO:
+                                    }, isSecureField: true)
+
+                }
+                .regular(size: 18)
+                .padding(EdgeInsets(top: 50, leading: 20, bottom: 70, trailing: 20))
+                
+                VStack(spacing: 15) {
+                    Button(action: {}, label: {
+                        Text("Sign In")
+                            .bold(size: 18)
+                            .frame(maxWidth: .infinity, minHeight: 50)
+                            .background(Color(#colorLiteral(red: 0.9607843137, green: 0.1764705882, blue: 0.337254902, alpha: 1)).opacity(0.7))
+                    })
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Forgot password?")
+                    })
+                }
+                .regular(size: 15)
+                .padding(.horizontal)
+                
+                Spacer()
+                
+                HStack {
+                    Text("Don't have an account?")
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Create one")
+                            .bold()
+                    })
+                }
+                .frame(maxWidth: .infinity)
+                .padding([.bottom, .horizontal], 15)
+                .regular(size: 15)
             }
         }
         .navigationBarHidden(true)
         .foregroundColor(.white)
-        .onAppear(perform: {
-//            UIApplication.shared.statusBarStyle = .lightContent
-        })
     }
 }
 
