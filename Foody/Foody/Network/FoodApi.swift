326c04e5d9b3ada41fc62b11c13e43edff6d8db6
An Nguyen T <an.nguyen2@monstar-lab.com>
2021-04-07 16:44:31 +0700

    Demo images picker

--- Foody/Foody/Network/FoodApi.swift
+++ Foody/Foody/Network/FoodApi.swift
@@ -18,53 +18,53 @@ enum FoodApi {
     case endPointWithLotsOfParams(parameters: Parameters)
 }
 
-extension FoodApi: TargetType {
-    var baseURL: URL {
-        guard let url = URL(string: "https://api.themoviedb.org/3/movie/") else {
-            fatalError("baseURL could not be configured.")
-        }
-        return url
-    }
-    
-    var path: String {
-        switch self {
-        case .recommended(let id):
-            return "\(id)/recommendations"
-        case .popular:
-            return "popular"
-        case .newMovies:
-            return "now_playing"
-        case .video(let id):
-            return "\(id)/videos"
-        case .endPointWithLotsOfParams(parameters: let parameters):
-            return "/videos"
-        }
-    }
-    
-    var method: Method {
-        .get
-    }
-    
-    var sampleData: Data {
-        Data()
-    }
-    
-    var task: Task {
-        switch self {
-        case .recommended, .video:
-            return .requestParameters(parameters: ["api_key": NetworkManager.MovieAPIKey], encoding: URLEncoding.queryString)
-        case .popular(let page), .newMovies(let page):
-            return .requestParameters(parameters: ["page": page, "api_key": NetworkManager.MovieAPIKey], encoding: URLEncoding.queryString)
-        case .endPointWithLotsOfParams(parameters: let parameters):
-            return .requestParameters(parameters:  parameters, encoding: URLEncoding.queryString)
-        }
-    }
-    
-    var headers: [String : String]? {
-        return ["Content-type": "application/json"]
-    }
-    
-}
+//extension FoodApi: TargetType {
+//    var baseURL: URL {
+//        guard let url = URL(string: "https://api.themoviedb.org/3/movie/") else {
+//            fatalError("baseURL could not be configured.")
+//        }
+//        return url
+//    }
+//    
+//    var path: String {
+//        switch self {
+//        case .recommended(let id):
+//            return "\(id)/recommendations"
+//        case .popular:
+//            return "popular"
+//        case .newMovies:
+//            return "now_playing"
+//        case .video(let id):
+//            return "\(id)/videos"
+//        case .endPointWithLotsOfParams(parameters: let parameters):
+//            return "/videos"
+//        }
+//    }
+//    
+//    var method: Method {
+//        .get
+//    }
+//    
+//    var sampleData: Data {
+//        Data()
+//    }
+//    
+//    var task: Task {
+//        switch self {
+//        case .recommended, .video:
+//            return .requestParameters(parameters: ["api_key": NetworkManager.MovieAPIKey], encoding: URLEncoding.queryString)
+//        case .popular(let page), .newMovies(let page):
+//            return .requestParameters(parameters: ["page": page, "api_key": NetworkManager.MovieAPIKey], encoding: URLEncoding.queryString)
+//        case .endPointWithLotsOfParams(parameters: let parameters):
+//            return .requestParameters(parameters:  parameters, encoding: URLEncoding.queryString)
+//        }
+//    }
+//    
+//    var headers: [String : String]? {
+//        return ["Content-type": "application/json"]
+//    }
+//    
+//}
 
 
 
