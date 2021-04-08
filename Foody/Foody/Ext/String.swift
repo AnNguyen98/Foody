73439d33d14ac27ec5e34b077626cc2e62d4de60
An Nguyen T <an.nguyen2@monstar-lab.com>
2021-04-08 15:56:55 +0700

    Rename some file

--- Foody/Foody/Ext/String.swift
+++ Foody/Foody/Ext/String.swift
@@ -32,3 +32,29 @@ extension String {
         return result
     }
 }
+
+enum Process {
+    case encode
+    case decode
+}
+
+extension String {
+    var len: Int { return count }
+    var host: String? { return (try? asURL())?.host }
+
+    func base64(_ method: Process) -> String {
+        switch method {
+        case .encode:
+            guard let data = data(using: .utf8) else { return "" }
+            return data.base64EncodedString()
+        case .decode:
+            guard let data = Data(base64Encoded: self),
+                let string = String(data: data, encoding: .utf8) else { return "" }
+            return string
+        }
+    }
+
+    var trimmed: String {
+        return trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+    }
+}
---
+++ Foody/Foody/Model/FirebaseManager/Firebase.Auth.swift
@@ -0,0 +1,12 @@
+//
+//  Firebase.Auth.swift
+//  Foody
+//
+//  Created by MBA0283F on 4/8/21.
+//
+
+import Foundation
+
+extension FirebaseManager.Auth {
+    
+}
