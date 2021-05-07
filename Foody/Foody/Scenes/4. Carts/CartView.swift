2a0f0577bb7845fa43ab5a3c87cdfd6135dfbc7a
annguyen98 <theannguyen98@gmail.com>
2021-05-07 07:12:42 +0700

    Implementing Carts View

--- Foody/Foody/Scenes/4. Carts/CartView.swift
+++ Foody/Foody/Scenes/4. Carts/CartView.swift
@@ -8,8 +8,85 @@
 import SwiftUI
 
 struct CartView: View {
+    @Environment(\.presentationMode) private var presentationMode
+    //@Environment(\.editMode)
+    @State private var editMode = false
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            LazyVStack {
+                ForEach(0..<20) { index in
+                    HStack {
+//                        if editMode {
+//                            Button(action: {
+//
+//                            }, label: {
+//                                Image(systemName: SFSymbols.trash)
+//                            })
+//                        }
+                        
+                        HStack {
+                            Image("food1")
+                                .resizable()
+                                .frame(width: 150, height: 90)
+                            
+                            VStack(spacing: 15) {
+                                HStack {
+                                    Text("\(index)")
+                                        .font(.title2)
+                                    
+                                    Spacer()
+                                    
+                                    Text("Today")
+                                        .foregroundColor(.gray)
+                                }
+                                Text("Order ID: \(UUID().uuidString)")
+                            }
+                            .padding(.trailing)
+                        }
+                        .font(.body)
+                        .lineLimit(1)
+                        .frame(maxWidth: kScreenSize.width)
+                        .background(Color.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .shadow(color: .gray, radius: 3, x: 2, y: 2)
+                        
+                        if editMode {
+                            Button(action: {
+                                
+                            }, label: {
+                                Image(systemName: SFSymbols.trash)
+                            })
+                        }
+                    }
+                    .animation(.default)
+                }
+            }
+//            .environment(\.editMode, $editMode)
+            .prepareForLoadMore(loadMore: {
+                
+            }, showIndicator: true)
+            .onRefresh {
+                
+            }
+            .navigationBarTitle("My Orders", displayMode: .inline)
+            .toolbar(content: {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: {
+                        presentationMode.dismiss()
+                    }, label: Image(systemName: SFSymbols.xmark))
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        editMode.toggle()
+                    }, label: Text(editMode ? "Done": "Edit"))
+                }
+            })
+            .onAppear(perform: {
+                UINavigationBar.appearance().tintColor = .gray
+            })
+        }
     }
 }
 
